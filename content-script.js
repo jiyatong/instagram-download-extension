909b5f3f9d33c960ead760845085f786673b1740
jiyt <15651300201@wo.cn>
2020-02-29 15:18:25 +0800

    v1.0

---
+++ content-script.js
@@ -0,0 +1,23 @@
+﻿// 接收popup的请求
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.cmd == 'download') {
+    sendResponse(findImg())
+  }
+})
+
+// 查找页面上的图片
+function findImg() {
+  var imgArr = []
+  var articles = document.getElementsByTagName('article')
+  Array.prototype.slice.call(articles).forEach(function (item) {
+    var srcArr = []
+    var imgs = Array.prototype.slice.call(item.getElementsByTagName('img'))
+    imgs.forEach(function (value) {
+      if (value.alt.indexOf('的头像') === -1) {
+        srcArr.push(value.src)
+      }
+    })
+    imgArr.push(...srcArr)
+  })
+  return imgArr
+}
---
+++ popup.js
@@ -0,0 +1,25 @@
+﻿function sendMessageToContentScript(message, callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+      if (callback) callback(response)
+    })
+  })
+}
+
+sendMessageToContentScript({ cmd: 'download', value: '获取图片地址' }, function (response) {
+  var app = document.getElementById('app')
+  response.forEach(function (item, index) {
+    var a = document.createElement('a')
+    a.href = item
+    a.target = '_blank'
+    var img = document.createElement('img')
+    img.src = item
+    img.width = 300
+    a.appendChild(img)
+    app.appendChild(a)
+  })
+})
+
+
+
+
